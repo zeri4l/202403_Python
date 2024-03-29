9244f2453d2241036d6f4f1bd18b758af5c7dc88
zeri4l <inst_h2av2n@naver.com>
2024-03-29 18:31:40 +0900

    name 묻기

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,23 @@
 print("hello world", end="")
-print('hello world')
+print('hello world', 'hello world', sep="123")
+score=100
+print(score)
+score=456
+score=123
+print(score)
+x=2
+y=2
+z= x * y
+print(z)
+# a= input('이름을 입력하세요: ')
+# print(a)
+# print('안녕하세요,', '님', sep="")
+# score=80
+# print('점수:', score)
+
+name= input('이름을 입력하세요: ')
+print('안녕하세요,', name, '님', sep="")
+
+            
+'''주석'''
+
