03989767b56c71b950eb97981fbc044f6086428d
zeri4l <inst_h2av2n@naver.com>
2024-05-22 14:24:49 +0900

    6일차 과제 수정

--- day06/과제1.py
+++ day06/과제1.py
@@ -1,17 +1,53 @@
 class Infor:
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
+    serialNumber = 0
+    def __init__(self, name, age, ismale, number):
+        self.__name = name
+        self.__age = age
+        self.__ismale = ismale
+        Infor.serialNumber += 1      
+        self.__number = Infor.serialNumber
         
+    def getName(self):
+        return self.__name
+    
+    def setAge(self, age):
+        if age >= 1:
+            self.__age = age
+        else:
+            print("나이는 한 살 이상이어야합니다.")
+            
+    
+    
+    def setIsmale(self, ismale):
+        if ismale == "남성":
+            ismale == True
+        else:
+            ismale == False
+            
+    def getIsmale(self):
+        if ismale == True:
+            return "남성"
+        else:
+            return "여성"
+       
+
+    
     def show(self):
-        print(f'내 이름은 {self.name}입니다. 나이는 {self.age}살, 성별은 {self.gender}입니다.')
+        print(f'내 이름은 {self.__name}입니다. 나이는 {self.__age}살, 성별은 {self.__ismale}입니다.')
         
     def plusAge(self):
-        self.age += 1 
+        self.__age += 1 
         
-a= Infor("1", 2, "male")
+    def check_number(self, number):
+        if number == Infor.serialNumber:
+            return True
+        else:
+            return False
+        
+        
+a= Infor("1", 2, "male", 1)
 
 a.show()
 a.plusAge()
 a.show()
+
